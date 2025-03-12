4badb40dcde640379c67f5c389c37e12129796b2
Sangjin Hwang <aosdb5640@naver.com>
2025-03-12 23:48:54 +0900

    add: uniform and resolution

--- src/components/BookOfShader/shader03.jsx
+++ src/components/BookOfShader/shader03.jsx
@@ -1,20 +1,37 @@
 import vertexShader from "../../shader/shader3/vertex.glsl";
 import fragmentShader from "../../shader/shader3/fragment.glsl";
-import { useMemo } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useEffect, useMemo, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
 
 const Shader3 = () => {
+  const { size } = useThree();
+  const materialRef = useRef();
   const uniforms = {
+    u_resolution: { value: new THREE.Vector2(size.width, size.height) },
     u_time: { value: 0 },
   };
 
   useFrame(({ clock }) => {
     uniforms.u_time.value = clock.getElapsedTime();
   });
+
+  useEffect(() => {
+    const updateResolution = () => {
+      if (materialRef.current) {
+        materialRef.current.uniforms.u_resolution.value.set(
+          size.width,
+          size.height
+        );
+      }
+    };
+    updateResolution();
+  }, [size]);
   return (
     <mesh>
       <planeGeometry />
       <shaderMaterial
+        ref={materialRef}
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
         uniforms={uniforms}
--- src/shader/shader3/fragment.glsl
+++ src/shader/shader3/fragment.glsl
@@ -2,8 +2,9 @@ precision mediump float;
 
 
 uniform float u_time;
+uniform vec2 u_resolution;
 
 void main() {
-
-  gl_FragColor = vec4(abs(sin(u_time)),0.0,0.0,1.0);
+  vec2 st = gl_FragCoord.xy/ u_resolution;
+  gl_FragColor = vec4(abs(sin(u_time)),st.x,st.y,1.0);
 }
